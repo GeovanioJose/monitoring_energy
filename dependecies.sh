68ac9693002912775950004561d7756af3589d1d
GeovanioJose <sgeojose@gmail.com>
2026-04-02 14:03:59 -0300

    initial commit

---
+++ dependecies.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+COMPILE_PODMAN() {
+  cd ~
+
+  apt install build-essential curl wget cmake gcc g++ -y
+
+  apt-get install -y \
+    libapparmor-dev \
+    btrfs-progs \
+    git \
+    iptables \
+    libassuan-dev \
+    libbtrfs-dev \
+    libc6-dev \
+    libdevmapper-dev \
+    libglib2.0-dev \
+    libgpgme-dev \
+    libgpg-error-dev \
+    libprotobuf-dev \
+    libprotobuf-c-dev \
+    libseccomp-dev \
+    libselinux1-dev \
+    libsystemd-dev \
+    pkg-config \
+    catatonit \
+    uidmap
+
+  apt-get install -y make git gcc build-essential pkgconf libtool \
+   libsystemd-dev libprotobuf-c-dev libcap-dev libseccomp-dev libyajl-dev \
+   go-md2man autoconf python3 automake
+
+  cd ~ || exit 1
+  git clone https://github.com/containers/crun.git
+  cd crun || exit 1
+  ./autogen.sh
+  ./configure CFLAGS='-I/usr/include/libseccomp'
+  make
+  make install
+
+  apt-get install netavark -y || apt-get install containernetworking-plugins -y
+
+  cd ~ || exit 1
+  #Addes go lang
+  wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+  tar -xzf go1.22.4.linux-amd64.tar.gz -C /usr/local
+  rm go1.22.4.linux-amd64.tar.gz
+  echo "export PATH=\$PATH:/usr/local/go/bin" >> $HOME/.profile
+  export PATH=$PATH:/usr/local/go/bin
+
+  cd ~
+
+  git clone https://github.com/containers/conmon
+  cd conmon
+  export GOCACHE="$(mktemp -d)"
+  make
+  make podman
+
+
+  mkdir -p /etc/containers
+
+  cat <<EOF >/etc/containers/policy.json
+{
+  "default": [
+    {
+      "type": "insecureAcceptAnything"
+    }
+  ]
+}
+EOF
+
+  cd ~
+
+  git clone https://github.com/containers/podman/
+  cd podman || exit 1
+  git checkout v5.1
+  make BUILDTAGS="selinux seccomp exclude_graphdriver_devicemapper systemd" PREFIX=/usr
+  make install PREFIX=/usr
+
+  cd ~
+
+  apt install -y protobuf-compiler
+
+  curl https://sh.rustup.rs -sSf | sh
+  source $HOME/.cargo/env
+
+  cd ~
+  git clone https://github.com/containers/netavark.git
+  cd netavark
+  make
+  cp bin/* /usr/local/libexec/podman/
+
+  podman --version
+}
+
+COMPILE_PODMAN
+
---
+++ start_teastore.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+ip_address=$(hostname -I | awk '{print $1}')
+
+software=$1
+sleep_time=60
+
+$software run -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=10000" -p 10000:8080 -d --restart=always docker.io/descartesresearch/teastore-registry
+
+sleep $sleep_time
+
+$software run -p 3306:3306 -d --restart=always docker.io/descartesresearch/teastore-db
+
+sleep $sleep_time
+
+$software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=1111" -e "DB_HOST=$ip_address" -e "DB_PORT=3306" -p 1111:8080 -d --restart=always docker.io/descartesresearch/teastore-persistence
+
+sleep $sleep_time
+
+$software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=2222" -p 2222:8080 -d --restart=always docker.io/descartesresearch/teastore-auth
+
+sleep $sleep_time
+
+$software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=3333" -p 3333:8080 -d --restart=always docker.io/descartesresearch/teastore-recommender
+
+sleep $sleep_time
+$software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=4444" -p 4444:8080 -d --restart=always docker.io/descartesresearch/teastore-image
+
+sleep $sleep_time
+
+$software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=8080" -p 8080:8080 -d --restart=always docker.io/descartesresearch/teastore-webui
